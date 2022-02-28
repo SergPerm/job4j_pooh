ba6970cfc4da53f3e891bed1f53219d89d9ec069
SergPerm <mohin@list.ru>
2022-02-28 19:50:47 +0500

    проект "Pooh JMS" [#268841 #269128]

--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -11,35 +11,34 @@ public class TopicService implements Service {
 
     @Override
     public Resp process(Req req) {
-        Resp result  = new Resp("Bad request", "400");
-        if ("POST".equals(req.httpRequestType())) {
-            String topicName = req.getSourceName();
-            String text = req.getParam();
-            var idInTopic = storage.get(topicName);
-            if (idInTopic == null) {
-                idInTopic = new ConcurrentHashMap<>();
-                storage.putIfAbsent(topicName, idInTopic);
-            }
-            for (Map.Entry<String, ConcurrentLinkedQueue<String>> id : idInTopic.entrySet()) {
-                id.getValue().add(text);
-            }
-            result = new Resp("Post added", "200");
-        }
-        if ("GET".equals(req.httpRequestType())) {
-            String topicName = req.getSourceName();
-            String id = req.getParam();
-            var idInTopic = storage.get(topicName);
-            if (idInTopic == null) {
-                idInTopic = new ConcurrentHashMap<>();
-                storage.putIfAbsent(topicName, idInTopic);
-            }
-            ConcurrentLinkedQueue<String> queue = idInTopic.get(id);
-            if (queue == null) {
-                idInTopic.put(id, new ConcurrentLinkedQueue<>());
+        Resp result;
+        switch (req.httpRequestType()) {
+            case "POST" -> {
+                String topicName = req.getSourceName();
+                String text = req.getParam();
+                var idInTopic = storage.get(topicName);
                 result = new Resp("", "204");
-            } else {
-                String text = queue.poll();
-                result = new Resp(text, "200");
+                if (idInTopic != null) {
+                    for (Map.Entry<String, ConcurrentLinkedQueue<String>> id : idInTopic.entrySet()) {
+                        id.getValue().add(text);
+                    }
+                    result = new Resp("", "200");
+                }
+            }
+            case "GET" -> {
+                String topicName = req.getSourceName();
+                String id = req.getParam();
+                storage.putIfAbsent(topicName, new ConcurrentHashMap<>());
+                storage.get(topicName).putIfAbsent(id, new ConcurrentLinkedQueue<>());
+                String text = storage.get(topicName).get(id).poll();
+                if (text == null) {
+                    result = new Resp("", "204");
+                } else {
+                    result = new Resp(text, "200");
+                }
+            }
+            default -> {
+                result  = new Resp("Not implemented", "400");
             }
         }
         return result;
